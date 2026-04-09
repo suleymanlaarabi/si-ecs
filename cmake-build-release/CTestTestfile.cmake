c0222012371d769471b14ace5bfe4662fa700e75
Suleyman Laarabi <suleyman.laarabi@epitech.eu>
2026-04-09 20:16:00 +0200

    remove temp file

--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/suleyman/CLionProjects/untitled
-# Build directory: /home/suleyman/CLionProjects/untitled/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[ecs_tests]=] "/home/suleyman/CLionProjects/untitled/cmake-build-release/ecs_tests")
-set_tests_properties([=[ecs_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/suleyman/CLionProjects/untitled/CMakeLists.txt;81;add_test;/home/suleyman/CLionProjects/untitled/CMakeLists.txt;0;")
